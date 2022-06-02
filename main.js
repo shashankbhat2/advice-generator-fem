0f9a703634c26ead1b19aceed81e6a4a3520223f
Shashank Bhat <shashank.bhat2525@gmail.com>
2022-06-02 19:37:25 +0530

    final

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
 //IIFE - Immediately Invoked Function Expression
 (async function () {
-  let advice_number = document.getElementById("advice_number");
-  let advice_content = document.getElementById("advice_content");
-  let advice_circle = document.getElementById("advice_circle");
-  let dice = document.getElementById("dice");
+  let adviceNumber = document.getElementById("advice_number");
+  let adviceContent = document.getElementById("advice_content");
+  let adviceCircle = document.getElementById("advice_circle");
+  let adviceDice = document.getElementById("dice");
   async function getAdvice() {
     let result = await fetch("https://api.adviceslip.com/advice");
     return result.json();
   }
   async function generateAdvice() {
     let advice = await getAdvice();
-    advice_number.innerHTML = `Advice #${advice.slip.id}`;
-    advice_content.innerHTML = `"${advice.slip.advice}"`;
+    adviceNumber.innerHTML = `Advice #${advice.slip.id}`;
+    adviceContent.innerHTML = `"${advice.slip.advice}"`;
   }
-  window.onload = generateAdvice();
-  advice_circle.addEventListener("click", () => {
-    dice.classList.add("rotate");
+  window.onload = await generateAdvice();
+  adviceCircle.addEventListener("click", () => {
+    adviceDice.classList.add("rotate");
     generateAdvice();
     setTimeout(function () {
       dice.classList.remove("rotate"); 
